186c7b403e10ed308990520f4d25246f2e518b66
Farsane <132861096+Farsane@users.noreply.github.com>
2024-05-12 00:25:29 +0100

    Suite Frontend

--- frontend-ang/src/app/app.module.ts
+++ frontend-ang/src/app/app.module.ts
@@ -11,11 +11,28 @@ import {MatIcon, MatIconModule} from "@angular/material/icon";
 import {MatMenu, MatMenuModule} from "@angular/material/menu";
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatList, MatListModule} from "@angular/material/list";
+import { HomeComponent } from './home/home.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoadStudentsComponent } from './load-students/load-students.component';
+import { LoadPaymentsComponent } from './load-payments/load-payments.component';
+import { LoginComponent } from './login/login.component';
+import { StudentsComponent } from './students/students.component';
+import { PaymentsComponent } from './payments/payments.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {MatCard, MatCardModule} from "@angular/material/card";
 
 @NgModule({
   declarations: [
     AppComponent,
-    AdminTemplateComponent
+    AdminTemplateComponent,
+    HomeComponent,
+    ProfileComponent,
+    LoadStudentsComponent,
+    LoadPaymentsComponent,
+    LoginComponent,
+    StudentsComponent,
+    PaymentsComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
@@ -25,7 +42,8 @@ import {MatList, MatListModule} from "@angular/material/list";
     MatIconModule,
     MatMenuModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [
     provideAnimationsAsync()
